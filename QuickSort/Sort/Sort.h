f22e7ea13136bf68d5f2d88af7dfe7bf673ebb4e
HIROaaaa <3311427495@qq.com>
2026-02-14 18:53:21 +0800

    Add files via upload

---
+++ QuickSort/Sort/Sort.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<stdbool.h>
+#include<time.h>
+typedef int HPDataType;
+typedef struct Heap
+{
+	HPDataType* a;
+	int size;
+	int capacity;
+}HP;
+typedef int SDataType;
+void AdjustDown(HPDataType* a, int n, int parent);
+void InsertSort(SDataType* a, int n);
+void Swap(HPDataType* a, HPDataType* b);
+void HeapSort(int* a, int n);
+void BubbleSort(int* a, int n);
+void ShellSort(int *a2,int N);
+void SelectSort(int *a3,int N);
+int GetMidi(int* a, int left, int right);
+void QuickSort(int* a, int left, int right);
+void QuickSortNonR(int* a, int left, int right);
