896999b21b5038f0c0f503418fb9d2b35b161852
HIROaaaa <3311427495@qq.com>
2026-02-19 17:29:33 +0800

    Add files via upload

---
+++ CountSort/Sort/Sort.c
@@ -0,0 +1,400 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Sort.h"
+void AdjustDown(HPDataType* a, int n, int parent)
+{
+	int child = parent * 2 + 1;
+	while (child < n)
+	{
+		if (child + 1 < n && a[child] < a[child + 1])
+		{
+			child++;
+		}
+		if (a[child] > a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+void InsertSort(SDataType* a, int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int end = i;
+		int tmp = a[end + 1];
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end+1] = a[end];
+				end--;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end+1] = tmp;
+	}
+}
+void Swap(HPDataType* a, HPDataType* b)
+{
+	HPDataType tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+void HeapSort(int* a, int n)
+{
+	/*for (int i = 1; i < n; i++)
+	{
+		AdjustUp(a, i);
+	}*/
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(a, n, i);
+	}
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		end--;
+	}
+}
+void BubbleSort(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int flag = 0;
+		for (int j = 0; j < n-i-1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				Swap(&a[j], &a[j + 1]);
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+}
+void ShellSort(int* a2, int N)
+{
+	int gap = N;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0; i < N - gap; i++)
+		{
+			int end = i;
+			int tmp = a2[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a2[end])
+				{
+					a2[end + gap] = a2[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a2[end + gap] = tmp;
+		}
+	}
+}
+void SelectSort(int* a3,int N)
+{
+	int begin = 0, end = N - 1;
+	int max = end, min = begin;
+	while (begin < end)
+	{
+		max = end;
+		min = begin;
+		for (int j = begin + 1; j <= end; j++)
+		{
+			if (a3[j] < a3[min])
+			{
+				min = j;
+			}
+			if (a3[j] > a3[max])
+			{
+				max = j;
+			}
+		}
+		Swap(&a3[min], &a3[begin]);
+		if (max == begin)
+		{
+			max = min;
+		}
+		Swap(&a3[max], &a3[end]);
+		begin++;
+		end--;
+	}
+}
+int GetMidi(int* a, int left, int right)
+{
+	int midi = (left + right) / 2;
+	if (a[midi] > a[left])
+	{
+		if (a[midi] < a[right])
+		{
+			return midi;
+		}
+		else if (a[left] > a[right])
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+	else
+	{
+		if (a[midi] > a[right])
+		{
+			return midi;
+		}
+		else if (a[right] < a[left])
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+}
+int PartSort1(int* a, int left, int right)
+{
+	int keyi = left;
+	int begin = left, end = right;
+	int midi = GetMidi(a, left, right);
+	Swap(&a[midi], &a[keyi]);
+	while (begin < end)
+	{
+		while (begin < end && a[keyi] <= a[end])
+		{
+			end--;
+		}
+		while (begin < end && a[keyi] >= a[begin])
+		{
+			begin++;
+		}
+		Swap(&a[begin], &a[end]);
+	}
+	Swap(&a[keyi], &a[begin]);
+	return begin;
+}
+int PartSort2(int* a, int left, int right)
+{
+	int keyi = left;
+	int prev = left;
+	int cur = prev + 1;
+	while (cur <= right)
+	{
+		if (a[keyi] > a[cur]&&++prev!=cur)
+		{
+			Swap(&a[prev], &a[cur]);
+		}
+		cur++;
+	}
+	Swap(&a[keyi], &a[prev]);
+	return prev;
+}
+void QuickSort(int* a, int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	if (right - left + 1 < 10)
+	{
+		InsertSort(a + left, right - left + 1);
+	}
+	else
+	{
+		int keyi = PartSort1(a, left, right);
+		//int keyi=PartSort2(a, left, right);
+		QuickSort(a, left, keyi - 1);
+		QuickSort(a, keyi + 1, right);
+	}
+	
+}
+#include"Stack.h"
+void QuickSortNonR(int* a, int left, int right)
+{
+	ST st;
+	STInit(&st);
+	STPush(&st, right);
+	STPush(&st, left);
+	while (!STEmpty(&st))
+	{
+		int begin = STTop(&st);
+		STPop(&st);
+		int end = STTop(&st);
+		STPop(&st);
+		int keyi = PartSort2(a, begin, end);
+		if (keyi+1 < end)
+		{
+			STPush(&st, end);
+			STPush(&st, keyi+1);
+		}
+		if (begin < keyi-1)
+		{
+			STPush(&st, keyi-1);
+			STPush(&st, begin);
+		}
+	}
+	STDestory(&st);
+}
+
+//时间复杂度O(N*logN)
+//空间复杂度0(N)
+void _MergeSort(int* a, int* tmp, int begin, int end)
+{
+	if (begin == end)
+	{
+		return;
+	}
+	int mid = (begin + end) / 2;
+	_MergeSort(a, tmp, begin, mid);
+	_MergeSort(a, tmp, mid + 1, end);
+
+	//归并
+	int begin1 = begin, end1 = mid;
+	int begin2 = mid + 1, end2 = end;
+	int i = begin;
+	while (begin1 <= end1 && begin2 <= end2)
+	{
+		if (a[begin1] < a[begin2])
+		{
+			tmp[i++] = a[begin1++];
+		}
+		else
+		{
+			tmp[i++] = a[begin2++];
+		}
+	}
+	while (begin1 <= end1)
+	{
+		tmp[i++] = a[begin1++];
+	}
+	while (begin2 <= end2)
+	{
+		tmp[i++] = a[begin2++];
+	}
+	memcpy(a+begin, tmp+begin, (end - begin + 1) * sizeof(int));
+}
+void MergeSort(int* a, int n)
+{
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	if (tmp == NULL)
+	{
+		perror("malloc fail");
+		return;
+	}
+	_MergeSort(a, tmp, 0, n - 1);
+
+	free(tmp);
+	tmp = NULL;
+}
+void MergeSortNonR(int* a, int n)
+{
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	if (tmp == NULL)
+	{
+		perror("malloc fail");
+		return;
+	}
+
+	//gap是每组的数据个数
+	int gap = 1;
+	while (gap < n)
+	{
+		for (int i = 0; i < n; i += gap * 2)
+		{
+			int begin1 = i, end1 = i + gap - 1;
+			int begin2 = i + gap, end2 = i + 2 * gap - 1;
+			if (begin2 >= n)
+			{
+				break;
+			}
+			if (end2 >= n)
+			{
+				end2 = n - 1;
+			}
+			int j = i;
+			while (begin1 <= end1 && begin2 <= end2)
+			{
+				if (a[begin1] < a[begin2])
+				{
+					tmp[j++] = a[begin1++];
+				}
+				else
+				{
+					tmp[j++] = a[begin2++];
+				}
+			}
+			while (begin1 <= end1)
+			{
+				tmp[j++] = a[begin1++];
+			}
+			while (begin2 <= end2)
+			{
+				tmp[j++] = a[begin2++];
+			}
+			memcpy(a + i, tmp + i, (end2 - i+1) * sizeof(int));
+		}
+		gap *= 2;
+	}
+	free(tmp);
+	tmp = NULL;
+}
+void CountSort(int* a, int n)
+{
+	int max = a[0];
+	int min = a[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] < min)
+		{
+			min = a[i];
+		}
+		if (a[i] > max)
+		{
+			max = a[i];
+		}
+	}
+	int range = max - min + 1;
+	int* count = (int*)calloc(range, sizeof(int));
+	if (count == NULL)
+	{
+		perror("calloc fail");
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		count[a[i]-min]++;
+	}
+	int j = 0;
+	for (int i = 0; i < range; i++)
+	{
+		while (count[i]--)
+		{
+			a[j++] = i+min;
+		}
+	}
+	free(count);
+	count = NULL;
+}
